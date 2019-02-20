0c56aae19535b7ca598fea1ee828dc07d766ff88
Danny <38340314+moo32@users.noreply.github.com>
2019-02-20 13:34:34 -0500

    Update Manager.java

--- RP/src/Main/Manager.java
+++ RP/src/Main/Manager.java
@@ -1,9 +1,55 @@
 package Main;
 
+import GeneticAlgorithm.FunctionGA;
+
 public class Manager 
 {
+	
+	double GAMutationRate = .01;
+	int GAPopulationSize = 100;
+	
+	//    public Swarm (int particles1, int function, double inertia, double cognitiveComponent, double socialComponent) {
+	int numOfParticles;
+	double inertia = ;
+	double cognitiveComponent = ;
+	double socialComponent = ;
+	
+	
+	
 	public static void main(String[] args)
 	{
+		//100 percent GA
+		
+		//100 percent PSO
+		
+		//GAFirst
+		
+	}
+	
+	
+	public void GAFirstPSO(double GAPercentage, double PSOPercentage, int iterations)
+	{
+		FunctionGA GA = new FunctionGA(.01,)
+		for(int i = 0; i < iterations*GAPercentage; i++)
+		{
+			
+		}
+		for(int i = 0; i < iterations*PSOPercentage; i++)
+		{
+			
+		}
+	}
+	
+	public void PSOFirstGA(double GAPercentage, double PSOPercentage, int interations, int iterations)
+	{
+		for(int i = 0; i < iterations*PSOPercentage; i++)
+		{
+			
+		}
+		for(int i = 0; i < iterations*GAPercentage; i++)
+		{
+			
+		}
 		
 	}
 }
