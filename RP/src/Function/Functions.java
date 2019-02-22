49d0285c2b83bdcd18b85fe824badc771d1676d0
Danny <38340314+moo32@users.noreply.github.com>
2019-02-22 14:40:50 -0500

    changed the bounds from IntMAX to 10

--- RP/src/Function/Functions.java
+++ RP/src/Function/Functions.java
@@ -52,7 +52,7 @@ public class Functions
     {
         if(x < 0 || x < lowRange || x > highRange)
         {
-            return Integer.MAX_VALUE;
+            return 10;
         }
         else
             return Math.pow(x, (x+5)/4)+Math.cos(9*x*x-Math.sin(x));
@@ -62,7 +62,7 @@ public class Functions
     {
         if(x < lowRange || x > highRange)
         {
-            return Integer.MAX_VALUE;
+            return 10;
         }
         else
             return (Math.sin(10 * Math.PI * x)/2*x)+Math.pow((x-1), 4);
@@ -72,7 +72,7 @@ public class Functions
     {
         if(x < lowRange || x > highRange)
         {
-            return Integer.MAX_VALUE;
+            return 10;
         }
         else
             return (x*x)+Math.pow(Math.sin(8*x-9), 4);
@@ -82,7 +82,7 @@ public class Functions
     {
         if(x < lowRange || x > highRange)
         {
-            return Integer.MAX_VALUE;
+            return 10;
         }
         else
             return x*x + -1* Math.cos(Math.pow((4*x),2));
